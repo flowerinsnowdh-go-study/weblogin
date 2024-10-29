ca892eedc09595f04bf059cde5eb79e769b21cbe
flowerinsnow <flowerimsnow@gmail.com>
2024-10-29 14:00:08 +0800

    init: 完成数据库表

--- controller/login.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "net/http"
-
-func (c *Controller) ControlLogin(w http.ResponseWriter, r *http.Request) {
-	c.Mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-
-	})
-}
